33b72a705450fde70c9c8450fbc66c186d10d88b
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-09 11:22:57 +0100

    added code to my script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let heading = document.querySelector("header");
+let footer = document.querySelector("footer");
+let body = document.querySelector("body");
+let omMig = {
+    persondata: {
+      cpr: "123456-7890",
+      navne: {
+        fornavn: "Jesper",
+        efternavn: "Hansen",
+      },
+      adresse: "Kærsangervej XX",
+      postnummer: 4300,
+      telefon: "32-32-32-32",
+    },
+    cevilstand: "Single",
+  };
