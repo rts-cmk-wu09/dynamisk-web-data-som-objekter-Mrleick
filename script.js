da8c56851639477fec1d4e121228396554e62779
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-09 12:24:26 +0100

    Updated code to script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 let heading = document.querySelector("header");
 let footer = document.querySelector("footer");
 let body = document.querySelector("body");
-let info = {
+let omMig = {
+    påklædning: ["Sokker", "bukser", "trøje", "cap", "underbukser", "undertrøje"],
     persondata: {
       cpr: "123456-7890",
       navne: {
-        fornavn: "Jesper",
-        efternavn: "Hansen",
+        fornavn: "Troels",
+        efternavn: "Rasmussen",
       },
       adresse: "Kærsangervej XX",
       postnummer: 4300,
       telefon: "32-32-32-32",
     },
-    cevilstand: "Single",
+    cevilstand: "fanget!",
+    børn: [
+      {
+        navn: "Freja",
+        køn: "Pige",
+        vaccineret: true,
+        veninder: ["Laura", "Anna", "Leonora"],
+      },
+      {
+        navn: "Carl-Emil",
+        køn: "dreng",
+        vaccineret: true,
+        venner: ["Morris", "August", "Luca"],
+      },
+    ],
+    pensionsalder: undefined,
   };
