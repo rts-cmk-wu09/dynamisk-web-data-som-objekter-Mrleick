24d9b4fef9c40706845389451159122ecff543c1
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-09 12:54:54 +0100

    Changed script name from script to data

---
+++ data.js
@@ -0,0 +1,27 @@
+let heading = document.querySelector("header");
+let footer = document.querySelector("footer");
+let body = document.querySelector("body");
+let omMig = {
+    påklædning: ["Sokker", "bukser", "trøje", "cap", "underbukser", "undertrøje"],
+    persondata: {
+        cpr: "123456-7890",
+        køn: "mand",
+        navne: {
+            fornavn: "Jesper",
+            efternavn: "Hansen",
+        },
+        adresse: "Taastrupgårdsvej",
+        postnummer: 2630,
+        telefon: "71 90 41 77",
+    },
+    cevilstand: "fanget!",
+    dyr: [
+        {
+            navn: "Freyja",
+            køn: "tæve",
+            vaccineret: true,
+            livretter: ["Laura", "Anna", "Leonora"],
+        },
+    ],
+    pensionsalder: undefined,
+};
