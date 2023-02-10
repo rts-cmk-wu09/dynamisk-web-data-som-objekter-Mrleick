1c0f43f5f64d9c9bcd56efbc177d1ad98c8124d5
Mrleick <104718595+Mrleick@users.noreply.github.com>
2023-02-10 09:26:57 +0100

    Added data to my h1 from data.js to index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+document.querySelector("h1").innerHTML = `
+${overskrift.titel}
+`;
+
+
+
+
+
 document.querySelector(".person").innerHTML = `
 <p>Fornavn: ${omMig.persondata.navne.fornavn}</p>
 <p>Efternavn: ${omMig.persondata.navne.efternavn}</p>
